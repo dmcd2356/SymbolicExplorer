55b8a92279039663e6a7ac3eb027b3c0f92567ab
Dan McDermet <dmcd2356@gmail.com>
2018-05-16 13:44:52 -0700

    updated for changes in danalyzer

--- explorer/Explorer.java
+++ explorer/Explorer.java
@@ -11,17 +11,19 @@ import com.microsoft.z3.Expr;
 import com.microsoft.z3.Solver;
 import com.microsoft.z3.Status;
 import com.microsoft.z3.BitVecNum;
-import static danalyzer.executor.Executor.z3Context;
-import static danalyzer.gui.DebugUtil.debugPrintSolve;
-import e1e4.Category7_vulnerable;
 import java.math.BigInteger;
-//import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+// the program to explore
+import e1e4.Category7_vulnerable;
+
 public class Explorer {
-  
+
+  // the symbolic parameter to track
+  private String symbolicParam = "e1e4/Category7_vulnerable.main([Ljava/lang/String;)V_1";
+
   private Solver z3Solver;
   public Context z3Context;
   private List<BoolExpr> z3Constraints;
@@ -30,18 +32,22 @@ public class Explorer {
   public Explorer() {
   }
 
-  public String findNewInput() {
+  public int getCostModel() {
+    return danalyzer.gui.DebugUtil.getInstructionCount();
+  }
+
+  public String fuzzer_bitFlip() {
     String result = "";    
 
     // get constraints from the Executor in the running program
     z3Constraints = danalyzer.executor.Executor.getConstraints();
     if (z3Constraints.size() < 1) {
-      System.out.println("findNewInput: No constraints found!");
+      System.out.println("fuzzer_bitFlip: No constraints found!");
       return result;
     }
 
     // create new list of constraints and copy all but the last constraint from previous run
-    z3Context = danalyzer.executor.Executor.z3Context;
+    z3Context = danalyzer.executor.Executor.getZ3Context();
     z3Solver = z3Context.mkSolver();
     for (int i = 0; i < z3Constraints.size() - 1; i++) {
       z3Solver.add(z3Constraints.get(i));
@@ -57,11 +63,13 @@ public class Explorer {
       Status status = z3Solver.check();
 
       if (status == Status.SATISFIABLE) {
-        Expr expr = danalyzer.executor.Executor.exprMap.get("e1e4/Category7_vulnerable.main([Ljava/lang/String;)V_1");
+        // solvable - get next solution
+        Expr expr = danalyzer.executor.Executor.getSymbolicExpression(symbolicParam);
         result = z3Solver.getModel().eval(expr, false).toString();
         result = "" + new BigInteger(result).longValue();
         System.out.println(z3Solver.getModel().toString());
 
+        // check if we've done this before
         if (runningTimes.containsKey(result)) {
           z3Solver.add(z3Context.mkNot(z3Context.mkEq(expr, z3Solver.getModel().eval(expr, false))));
           continue;
@@ -88,13 +96,18 @@ public class Explorer {
     int instrCount = 0;
     
     while (!currInput.equals("")) {
-      System.out.println("------------------ Running Category7 with input = " + currInput);
-      try {        
+      System.out.println("------------------ Running test with input = " + currInput);
+      try {
+        // run program with current input value
         e1e4.Category7_vulnerable.main(new String[]{currInput});
-        instrCount = danalyzer.gui.DebugUtil.getInstructionCount();
+
+        // get the cost for the last run and map it to the input used to derrive it
+        instrCount = getCostModel();
         runningTimes.put(currInput, instrCount);
         System.out.println("input: " + currInput + ", instruction count: " + instrCount);
-        currInput = findNewInput();
+
+        // get next input value from solver
+        currInput = fuzzer_bitFlip();
       } catch (InterruptedException e) {
         System.err.println("Interrupted: " + e.getStackTrace());
       }
